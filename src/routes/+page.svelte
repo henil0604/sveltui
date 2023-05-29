9a17831bcdb7b45476de8761e31268385444750a
Henil Malaviya <henilmalaviya06@gmail.com>
2023-05-29 20:27:27 +0530

    chore: added success example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
         <Button variant="primary" type="filled">Hello World</Button>
         <Button variant="secondary" type="filled">Hello World</Button>
         <Button variant="tertiary" type="filled">Hello World</Button>
+        <Button variant="success" type="filled">Hello World</Button>
         <Button variant="dark" type="filled">Hello World</Button>
     </div>
 
@@ -15,6 +16,7 @@
         <Button variant="primary" type="outlined">Hello World</Button>
         <Button variant="secondary" type="outlined">Hello World</Button>
         <Button variant="tertiary" type="outlined">Hello World</Button>
+        <Button variant="success" type="outlined">Hello World</Button>
         <Button variant="dark" type="outlined">Hello World</Button>
     </div>
 
@@ -22,6 +24,7 @@
         <Button variant="primary" type="soft">Hello World</Button>
         <Button variant="secondary" type="soft">Hello World</Button>
         <Button variant="tertiary" type="soft">Hello World</Button>
+        <Button variant="success" type="soft">Hello World</Button>
         <Button variant="dark" type="soft">Hello World</Button>
     </div>
 </div>
