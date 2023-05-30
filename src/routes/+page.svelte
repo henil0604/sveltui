0dec32240b62381b28e6e1bf809e0f2c29223a3d
Henil Malaviya <henilmalaviya06@gmail.com>
2023-05-30 17:59:30 +0530

    chore: example of card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { Button, ButtonGroup, Spinner } from "$lib";
+    import { Button, ButtonGroup, Card, Spinner } from "$lib";
+    import LoadingOverlay from "$lib/components/LoadingOverlay/LoadingOverlay.svelte";
     import "../app.postcss";
 
     let loading = false;
@@ -10,12 +11,16 @@
 </script>
 
 <div class="flex flex-col flex-wrap gap-4 p-3">
-    <div class="flex flex-wrap gap-1">
-        <ButtonGroup>
-            <Button variant="error" type="soft">Day</Button>
-            <Button variant="error" type="soft">Week</Button>
-            <Button variant="error" type="soft">Month</Button>
-            <Button variant="error" type="soft">Year</Button>
-        </ButtonGroup>
-    </div>
+    <Card interactive type="filled" variant="error">
+        <svelte:fragment slot="header"
+            ><h1 class="text-xl">Hey!</h1></svelte:fragment
+        >
+        <svelte:fragment slot="body"
+            >Something really bad just happened!!!</svelte:fragment
+        >
+        <div class="flex justify-end mt-2 gap-1" slot="footer">
+            <Button type="soft" variant="tertiary">Ignore</Button>
+            <Button variant="warn">Report</Button>
+        </div>
+    </Card>
 </div>
