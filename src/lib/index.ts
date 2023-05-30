ec4955e289915c4f807db3e982cfce5a86057ba6
Henil Malaviya <henilmalaviya06@gmail.com>
2023-05-30 16:17:22 +0530

    chore: better import export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from "$lib/components/Button/Button.svelte";
-export { default as ButtonGroup } from "$lib/components/ButtonGroup/ButtonGroup.svelte";
-export { default as Spinner } from "$lib/components/Spinner/Spinner.svelte";
+export { default as Button } from '$lib/components/Button';
+export { default as ButtonGroup } from '$lib/components/ButtonGroup';
+export { default as Spinner } from '$lib/components/Spinner';
